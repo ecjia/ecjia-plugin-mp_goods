a462c9b9373654696b76f3572ada03bba537c65e
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-02-20 16:21:39 +0800

    更新推荐商品插件中的链接

--- mp_goods_best.class.php
+++ mp_goods_best.class.php
@@ -73,7 +73,7 @@ class mp_goods_best extends platform_abstract
     		$articles[$key]['Title'] = $val['goods_name'];
     		$articles[$key]['Description'] = '';
     		$articles[$key]['PicUrl'] = RC_Upload::upload_url($val['goods_img']);
-    		$articles[$key]['Url'] = 'http://test.b2b2c.ecjia.com/sites/touch/index.php?m=goods&c=index&a=init&id='.$val['goods_id'];
+    		$articles[$key]['Url'] = RC_Uri::home_url().'/sites/m/index.php?m=goods&c=index&a=show&goods_id='.$val['goods_id'];
     	}
     	$count = count($articles);
     	$content = array(
--- mp_goods_new.class.php
+++ mp_goods_new.class.php
@@ -69,20 +69,12 @@ class mp_goods_new extends platform_abstract
     public function event_reply() {
     	$goods_db = RC_Loader::load_app_model('goods_model','goods');
     	$data = $goods_db->where(array('is_new'=>1,'is_delete'=>0))->order('sort_order ASC')->limit(5)->select();
-    	 
     	$articles = array();
     	foreach ($data as $key => $val) {
     		$articles[$key]['Title'] = $val['goods_name'];
     		$articles[$key]['Description'] = '';
     		$articles[$key]['PicUrl'] = RC_Upload::upload_url($val['goods_img']);
-    		//$articles[$key]['Url'] = 'http://test.b2b2c.ecjia.com/sites/touch/index.php?m=goods&c=index&a=init&id='.$val['goods_id'];
-    		$home_url =  RC_Uri::home_url();
-    		if (strpos($home_url, 'sites')) {
-    			$url = substr($home_url, 0, strpos($home_url, 'sites'));
-    			$articles[$key]['Url'] = $url.'sites/m/index.php?m=goods&c=index&a=show&goods_id='.$val['goods_id'];
-    		} else {
-    			$articles[$key]['Url'] = $home_url.'/sites/m/index.php?m=goods&c=index&a=show&goods_id='.$val['goods_id'];
-    		}
+    		$articles[$key]['Url'] = RC_Uri::home_url().'/sites/m/index.php?m=goods&c=index&a=show&goods_id='.$val['goods_id'];
     	}
     	$count = count($articles);
     	$content = array(
